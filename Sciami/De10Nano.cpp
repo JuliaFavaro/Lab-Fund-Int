7b72ece704db8a0d8d63df4c79bf98c45195c096
Julia <j.favaro@studenti.unipi.it>
2024-12-12 12:05:32 +0100

    Modifiche al setup

--- Sciami/De10Nano.cpp
+++ Sciami/De10Nano.cpp
@@ -18,40 +18,62 @@
 #include "read_data10.h"
 #include "flux_graphs_10.h"
 
-int main() {
+int mainjulia() {
+    std::cout<<"Inizio"<<std::endl;
+
     // Nome del file di input
-    std::string filename = "Dati/FIFOread_20241203-172807.txt"; //questo funziona solo se Dati si trova in una sottocartella della principale
+    std::string filename = "Dati/FIFOread_20241203-172807.txt"; // Questo funziona solo se "Dati" si trova in una sottocartella della principale
 
-    // Vettori per memorizzare i numeri dei canali e i tempi
+    // Vettori per memorizzare i numeri dei canali e i tempi del file
     std::vector<int> decimalNumbers;
-    std::vector<double> tempi;
+    std::vector<double> counts;
+    readData(filename, decimalNumbers, counts);
 
-    // Leggi i dati dal file
-    readData(filename, decimalNumbers, tempi);
-    
-    // Tempo totale di acquisizione
+    // Sistema i tempi con i reset
+    std::vector<int> Numbers;
+    std::vector<double> tempi;
+    reset_clock(decimalNumbers, counts, Numbers, tempi);
+
+    // Calcola il tempo totale di acquisizione
     double time_start_total = *std::min_element(tempi.begin(), tempi.end());
     double time_end_total = *std::max_element(tempi.begin(), tempi.end());
     double total_time = time_end_total - time_start_total;
-    std::cout<<"Tempo totale in giorni "<<total_time/(3600*24)<<std::endl;
+    std::cout << "Tempo totale in giorni: " << total_time / (3600 * 24) << std::endl;
 
-    // Vettore di vettori per memorizzare i tempi di arrivo per ciascun canale
+     // Vettore di vettori per memorizzare i tempi di arrivo per ciascun canale
     std::vector<std::vector<double>> channelTimes(7);
 
     // Processa ciascun numero decimale per determinare i canali attivati
     for (size_t i = 0; i < decimalNumbers.size(); ++i) {
-        int decimalNumber = decimalNumbers[i];
-        std::vector<int> activeChannels = getActiveChannels(decimalNumber);
+        int Number = Numbers[i];
+        std::vector<int> activeChannels = getActiveChannels(Number);
 
         // Aggiungi il tempo corrispondente ai canali attivati
         for (int channel : activeChannels) {
             channelTimes[channel].push_back(tempi[i]);
         }
     }
+    std::cout<<"Diviso in canali"<<std::endl;
 
     /*Legenda dei telescopi: il canale 2 è il canale del Setup06, il canale 5
     è il Setup08, mentre il canale 6 è il telescopio in Stanza2004.*/
-    // Intervallo di 10s
-    double interval = 10;
+    double interval = 30*60; //30 minuti
+    int num_intervals=static_cast<int>(total_time / interval);
+    Rategraph3(interval,num_intervals,channelTimes[1],channelTimes[3], channelTimes[6]);
+    std::cout<<"Calcolato la variazione di flusso nel tempo per i vari set "<<std::endl;
+
+    /*Consideriamo la variazione dell'efficienza nel tempo del Setup06:
+    Doppie 2 e 3: CH0, Doppie 1 e 2 CH1, Triple: CH2*/
+
+    std::vector<double> counts23 = channelTimes[0];
+    std::vector<double> counts12 = channelTimes[1];
+    std::vector<double> triple_06 = channelTimes[2];
+    
+    double interval_eff=3600;
+    int num_intervals_eff=static_cast<int>(total_time/interval_eff);
+
+    efficiency_set06(interval_eff, num_intervals_eff, counts23, counts12, triple_06);
+    std::cout<<"Calcolata la variazione dell'efficienza Setup06 "<<std::endl;
+
     return 0;
 }
--- Sciami/read_data10.h
+++ Sciami/read_data10.h
@@ -6,9 +6,11 @@
 #include <vector>
 #include <bitset>
 #include <iomanip>
+#include <cmath>
+
 
 // Funzione per leggere e formattare i dati dal file da De0Nano
-void readData(const std::string& filename, std::vector<int>& decimalNumbers, std::vector<double>& tempi) {
+void readData(const std::string& filename, std::vector<int>& decimalNumbers, std::vector<double>& counts) {
     // Apri il file
     std::ifstream file(filename);
 
@@ -24,24 +26,50 @@ void readData(const std::string& filename, std::vector<int>& decimalNumbers, std
     // Leggi i dati dal file
     while (file >> number >> tempo) {  //risoluzione temporale di 5 ns
         decimalNumbers.push_back(number);
-        tempi.push_back(tempo);
+        counts.push_back(tempo);
     }
 
     // Chiudi il file
     file.close();
 }
 
-// Funzione per determinare i canali attivati dato un numero decimale e per sistemare i tempi dopo il reset counter
-std::vector<int> getActiveChannels(int decimalNumber) { //reset counter del clock a 20^30 ns-> 5 s
+void reset_clock( std::vector<int>& decimalNumbers, std::vector<double>& counts, std::vector<int>& Number, std::vector <double>& tempi_sistemati){
+    int numero_reset=0;
+    int inizio_file=0;
+    int it=0;
+
+    //trovo il primo reset
+    while (true){
+        if (decimalNumbers[it]==static_cast<double>(524288)){
+            inizio_file=it;
+            break;
+        }
+        ++it;
+    }
+
+    int i=0;
+    for (i=inizio_file+1; i<decimalNumbers.size(); ++i){
+        if (decimalNumbers[i]==static_cast<double>(524288)){
+            ++numero_reset; //clock resettato
+            ++i; //salti questo elemento
+        }
+        else{
+            Number.push_back(decimalNumbers[i]);
+            tempi_sistemati.push_back(counts[i]*5e-9+numero_reset*5.36870912); //converto i counts in secondi
+        }
+    }
+}
+
+// Funzione per determinare i canali attivati dato un numero decimale 
+std::vector<int> getActiveChannels(int decimalNumber) {//reset counter del clock a 20^30 ns-> 5 s
     std::vector<int> activeChannels;
     std::bitset<7> bits(decimalNumber); // Assumiamo un sistema con 7 canali
-
+    
     for (int i = 0; i < 7; ++i) {
         if (bits[i]) {
             activeChannels.push_back(i);
         }
     }
-
     return activeChannels;
 }
 
